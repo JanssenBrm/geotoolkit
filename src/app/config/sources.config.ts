dd0df096d15771099d0abb52e0d5494f272357df
janssenb <bram.janssen@vito.be>
2020-10-06 09:51:47 +0200

    Added BCGMS as a source

--- src/app/config/sources.config.ts
+++ src/app/config/sources.config.ts
@@ -1,4 +1,3 @@
-
 export const SOURCES = [
     {
         'name': 'TerraScope (WMTS)',
@@ -7,33 +6,34 @@ export const SOURCES = [
         'infoUrl': '',
         'imageUrl': 'https://pbs.twimg.com/profile_images/935755572972007424/ggPzGIgG.jpg'
     },
-  {
-    'name': 'TerraScope (WMTS - V2)',
-    'description': 'VITO WMTS based on GeoTrellis',
-    'url': 'https://services.terrascope.be/wmts/v2/wmts?service=wmts&request=GetCapabilities',
-    'infoUrl': '',
-    'imageUrl': 'https://pbs.twimg.com/profile_images/935755572972007424/ggPzGIgG.jpg'
-  },
-  {
-    'name': 'GeoTrellis (WMTS - Dev)',
-    'description': 'VITO WMTS based on GeoTrellis',
-    'url': 'http://wmts01-dev.services.rscloud.vito.be:8080/service/wmts?REQUEST=getcapabilities',
-    'infoUrl': '',
-      'imageUrl': 'https://pbs.twimg.com/profile_images/935755572972007424/ggPzGIgG.jpg' },
-  {
-    'name': 'PROBA-V MEP (WMTS)',
-    'description': 'The  PROBA-V Mission Exploitation Platform (MEP), as an ESA pathfinder project, complements the PROBA-V user segment by offering an operational Exploitation Platform on the PROBA-V data, correlative data and derived products. ',
-    'url': 'https://proba-v-mep.esa.int/applications/geo-viewer/app/mapcache/wmts?service=WMTS&request=GetCapabilities',
-    'infoUrl': 'https://proba-v-mep.esa.int/',
-    'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
-  },
-  {
-      'name': 'MAPS - CECN (WMS)',
-      'description': 'VITO TAP Maps Cluster',
-      'url': 'http://maps.vgt.vito.be/geoserver/CECN/ows?service=wms&version=1.3.0&request=getcapabilities',
-      'infoUrl': '',
-      'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
-  },
+    {
+        'name': 'TerraScope (WMTS - V2)',
+        'description': 'VITO WMTS based on GeoTrellis',
+        'url': 'https://services.terrascope.be/wmts/v2/wmts?service=wmts&request=GetCapabilities',
+        'infoUrl': '',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/935755572972007424/ggPzGIgG.jpg'
+    },
+    {
+        'name': 'GeoTrellis (WMTS - Dev)',
+        'description': 'VITO WMTS based on GeoTrellis',
+        'url': 'http://wmts01-dev.services.rscloud.vito.be:8080/service/wmts?REQUEST=getcapabilities',
+        'infoUrl': '',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/935755572972007424/ggPzGIgG.jpg'
+    },
+    {
+        'name': 'PROBA-V MEP (WMTS)',
+        'description': 'The  PROBA-V Mission Exploitation Platform (MEP), as an ESA pathfinder project, complements the PROBA-V user segment by offering an operational Exploitation Platform on the PROBA-V data, correlative data and derived products. ',
+        'url': 'https://proba-v-mep.esa.int/applications/geo-viewer/app/mapcache/wmts?service=WMTS&request=GetCapabilities',
+        'infoUrl': 'https://proba-v-mep.esa.int/',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
+    },
+    {
+        'name': 'MAPS - CECN (WMS)',
+        'description': 'VITO TAP Maps Cluster',
+        'url': 'http://maps.vgt.vito.be/geoserver/CECN/ows?service=wms&version=1.3.0&request=getcapabilities',
+        'infoUrl': '',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
+    },
     {
         'name': 'MAPS - DCS4COP (WMS)',
         'description': 'VITO TAP Maps Cluster',
@@ -49,10 +49,17 @@ export const SOURCES = [
         'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
     },
     {
-    'name': 'MAPS (Geowebcache)',
-    'description': 'VITO TAP Maps Cluster',
-    'url': 'http://maps.vgt.vito.be/geowebcache/service/wmts?REQUEST=getcapabilities',
-    'infoUrl': '',
-    'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
-}
-  ];
+        'name': 'MAPS - BCGMS (WMS)',
+        'description': 'VITO TAP Maps Cluster',
+        'url': 'http://maps.vgt.vito.be/geoserver/BCGMS/ows?service=wms&version=1.3.0&request=getcapabilities',
+        'infoUrl': '',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
+    },
+    {
+        'name': 'MAPS (Geowebcache)',
+        'description': 'VITO TAP Maps Cluster',
+        'url': 'http://maps.vgt.vito.be/geowebcache/service/wmts?REQUEST=getcapabilities',
+        'infoUrl': '',
+        'imageUrl': 'https://pbs.twimg.com/profile_images/641168186897858560/PI1QimyT_400x400.png'
+    }
+];
