452a112ae23ec69ed6972ba6341633414de852d4
janssenb <bram.janssen@vito.be>
2020-12-09 09:43:17 +0100

    [FD-3335] Use a map on the contrast definition

--- src/app/components/sigmoidcontrast/sigmoid-contrast.component.ts
+++ src/app/components/sigmoidcontrast/sigmoid-contrast.component.ts
@@ -1,5 +1,15 @@
 import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import * as Highcharts from 'highcharts';
+import { transformExtent, transform } from 'ol/proj';
+import { getCenter } from 'ol/extent';
+import { Vector } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import { Map, View, Overlay } from 'ol';
+import { defaults, ZoomToExtent, ScaleLine} from 'ol/control';
+import { defaults as interactionDefaults} from 'ol/interaction';
+import { Tile } from 'ol/layer';
+import { OSM, WMTS } from 'ol/source';
+import {LayerService} from '../../services/layer.service';
 
 declare var require: any;
 const Boost = require('highcharts/modules/boost');
@@ -16,14 +26,13 @@ noData(Highcharts);
     templateUrl: './sigmoid-contrast.component.html',
     styleUrls: ['./sigmoid-contrast.component.scss']
 })
-export class SigmoidContrastComponent implements AfterViewInit {
+export class SigmoidContrastComponent implements OnInit {
 
     @Input()
     layer: any;
 
     @Input()
     extent: number[];
-
 
     @Output()
     confirm: EventEmitter<{ alpha: number, beta: number }> = new EventEmitter<{alpha: number; beta: number}>();
@@ -34,12 +43,14 @@ export class SigmoidContrastComponent implements AfterViewInit {
     alpha = 0.5;
     beta = 1;
 
-    originalLoaded = false;
-    contrastLoaded = false;
 
-    custom = '';
-    url = '';
-    contrast_url = '';
+    original_layer: Tile;
+    result_layer: Tile;
+
+    tilecounter = {
+        original: 0,
+        result: 0
+    };
 
     options: any = {
         chart: {
@@ -72,21 +83,116 @@ export class SigmoidContrastComponent implements AfterViewInit {
 
     chart: Highcharts.Chart;
 
-    constructor() {
+    constructor(
+        private layerService: LayerService
+    ) {
     }
 
-    ngAfterViewInit() {
+    ngOnInit() {
         if (this.layer.contrast.params.alpha && this.layer.contrast.params.beta) {
             this.alpha = +this.layer.contrast.params.alpha;
             this.beta = +this.layer.contrast.params.beta;
         }
-
-        const url = '{host}layer={layer}&style=&tilematrixset=EPSG%3A3857&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix=EPSG%3A3857%3A12&TileCol=2103&TileRow=1365&TIME=2020-11-18';
-        this.url = url
-            .replace('{host}', this.layer.layer.getSource().getUrls()[0])
-            .replace('{layer}', this.layer.layer.getSource().getLayer());
+        this.setupOriginalMap(this.layer.layer, this.extent);
         this.chart = Highcharts.chart('chart', this.options);
         this.updateSeries();
+    }
+
+    setupOriginalMap(layer: any, extent: number[]) {
+        this.original_layer = this.createLayer(layer, 'original');
+        this.createMap('current_map', this.original_layer, extent);
+    }
+
+    createLayer(layer: any, type: string): Tile {
+        const new_layer = this.copyLayer(layer);
+        new_layer.getSource().on('tileloadstart', () => {
+            this.tilecounter[type] += 1;
+        });
+        new_layer.getSource().on('tileloadend', () => {
+            this.tilecounter[type] -= 1;
+        });
+        new_layer.getSource().on('tileloaderror', () => {
+            this.tilecounter[type] -= 1;
+        });
+        return new_layer;
+    }
+
+    copyLayer(layer: any): Tile {
+        const source = layer.getSource();
+        return new Tile({
+            ...layer.getProperties(),
+            preload: 0,
+            source: new WMTS({
+                layer: source.getLayer(),
+                tileGrid: source.getTileGrid(),
+                projection: source.getProjection(),
+                dimensions: source.getDimensions(),
+                urls: source.getUrls(),
+                matrixSet: source.getMatrixSet(),
+            })
+        });
+    }
+
+    setupResultMap(layer: any, extent: number[]) {
+        if (!this.result_layer) {
+            this.result_layer = this.createLayer(layer, 'result');
+            this.createMap('result_map', this.result_layer, extent);
+        }
+
+        this.result_layer.getSource().setTileLoadFunction((tile, src) => this.layerService.loadTile(tile, src, {
+            contrast: {
+                enabled: true,
+                params: {
+                    alpha: this.alpha,
+                    beta: this.beta
+                }
+            }
+        }));
+        this.result_layer.getSource().refresh();
+
+    }
+
+    createMap(id: string, layer: any, extent: number[]): Map {
+        const center = getCenter(extent);
+        const map = new Map({
+            layers: [this.getBackgroundLayer(), layer],
+            maxTilesLoading: 128,
+            controls: defaults({
+                attribution: false,
+                rotate: false,
+                zoom: false
+            }),
+            interactions: interactionDefaults({
+                doubleClickZoom: false,
+                dragAndDrop: false,
+                keyboardPan: false,
+                keyboardZoom: false,
+                mouseWheelZoom: false,
+                dragPan: false,
+                pointer: false,
+                select: false,
+                shiftDragZoom: false
+            }),
+            view: new View({
+                projection: 'EPSG:3857',
+                center: center,
+                zoom: 12
+            }),
+            target: id
+        });
+
+        setTimeout(() => {
+            map.updateSize();
+        }, 200);
+        return map;
+    }
+
+    getBackgroundLayer() {
+        return new Tile({
+            id: 'background',
+            source: new OSM(),
+            visible: true
+        });
     }
 
     updateSeries() {
@@ -95,12 +201,11 @@ export class SigmoidContrastComponent implements AfterViewInit {
         }
         this.chart.addSeries(this.getSigmoidSeries(), true);
         this.chart.reflow();
-        this.updateUrls();
+        this.updateResult();
     }
 
-    updateUrls() {
-        this.contrastLoaded = false;
-        this.contrast_url = `${this.url}&alpha=${this.alpha}&beta=${this.beta}`;
+    updateResult() {
+        this.setupResultMap(this.layer.layer, this.extent);
     }
 
 
